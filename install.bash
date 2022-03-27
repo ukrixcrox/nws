9257560ab9dfc022b226782ed80e14b5733b587a
Falk-Witte <falk.witte15@gmail.com>
2022-03-27 09:33:27 +0200

    changed installation script

--- install.bash
+++ install.bash
@@ -13,8 +13,9 @@ wget https://raw.githubusercontent.com/Falk-Witte/nws/main/nws
 
 #install newspaper3k
 newspaper=~/.local/lib/python3.10/site-packages/newspaper/
+newspaper2=/usr/lib/python3.10/site-packages/newspaper/
 
-if [[ -d $newspaper ]]
+if [[ -d $newspaper ]] || [[ -d $newspaper2 ]]
 then
     echo "newspaper3k is installed"
 else
@@ -23,8 +24,9 @@ fi
 
 #install pyfiglet
 pyfiglet=/usr/lib/python3.10/site-packages/pyfiglet/
+pyfiglet2=~/.local/lib/python3.10/site-packages/pyfiglet/
 
-if [[ -a $pyfiglet ]]
+if [[ -d $pyfiglet ]] || [[ -d $pyfiglet2 ]]
 then
     echo "pyfiglet is installed"
 else
@@ -33,13 +35,15 @@ fi
 
 #install colorama
 colorama=~/.local/lib/python3.10/site-packages/colorama/
+colorama2=/usr/lib/python3.10/site-packages/colorama/
 
-if [[ -a $colorama ]]
+if [[ -d $colorama ]] || [[ -d $colorama2 ]]
 then
     echo "colorama is installed"
 else
     pip install colorama
 fi
 
+#self destruct after running
 rm -f $0
 
